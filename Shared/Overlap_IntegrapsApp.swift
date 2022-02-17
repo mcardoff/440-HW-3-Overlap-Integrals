c40e9673dc67ebf584792950d91f68bafe6dc029
Michael Cardiff <mcardiff@hawk.iit.edu>
2022-02-16 19:33:07 -0600

    Initial Commit

---
+++ Shared/Overlap_IntegrapsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Overlap_IntegrapsApp.swift
+//  Shared
+//
+//  Created by Michael Cardiff on 2/16/22.
+//
+
+import SwiftUI
+
+@main
+struct Overlap_IntegrapsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
