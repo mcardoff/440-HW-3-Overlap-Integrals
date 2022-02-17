86cf44489aef53e73495cc7ebb76ae1a68a21cf7
Michael Cardiff <mcardiff@hawk.iit.edu>
2022-02-17 15:38:14 -0600

    Change CV to do something

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -8,9 +8,19 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @ObservedObject var monteCarlo = MonteCarloCalculator()
+    
     var body: some View {
-        Text("Hello, world!")
+//        Text("Hello, world!")
+//            .padding()
+        Button("Bruh", action: self.calculate)
             .padding()
+            .frame(width: 100)
+    }
+    
+    func calculate() {
+        monteCarlo.monteCarloIntegrate(leftwavefunction: psi1s, rightwavefunction: psi1s, xMin: -10, yMin: -10, zMin: -10, xMax: 10, yMax: 10, zMax: 10, n: 10, spacing: 0)
     }
 }
 
