b01a074d62f717285375bcfd09238ba52b76d055
Michael Cardiff <mcardiff@hawk.iit.edu>
2022-02-18 10:25:49 -0600

    Add comments

--- Shared/Wavefunctions.swift
+++ Shared/Wavefunctions.swift
@@ -7,6 +7,11 @@
 
 import SwiftUI
 
+
+/// psi1s
+///
+/// 1s orbital in rectangular coordinates
+///
 func psi1s(x: Double, y: Double, z: Double) -> Double {
     let r = sqrt(x*x + y*y + z*z)
     let phi = convertPhi(x: x, y: y, z: z)
@@ -14,6 +19,10 @@ func psi1s(x: Double, y: Double, z: Double) -> Double {
     return psi1sSph(r: r, theta: theta, phi: phi)
 }
 
+/// psi2px
+///
+/// 2px orbital in rectangular coordinates
+///
 func psi2px(x: Double, y: Double, z: Double) -> Double {
     let r = sqrt(x*x + y*y + z*z)
     let phi = convertPhi(x: x, y: y, z: z)
@@ -21,12 +30,20 @@ func psi2px(x: Double, y: Double, z: Double) -> Double {
     return psi2pxSph(r: r, theta: theta, phi: phi)
 }
 
+/// psi1sSph
+///
+/// 1s orbital in spherical coordinates
+///
 func psi1sSph(r: Double, theta: Double, phi: Double) -> Double {
     let coeff = 1.0 / sqrt(Double.pi)
     let expon = exp(-r)
     return coeff * expon
 }
 
+/// psi2pxSph
+///
+/// 2px orbital in spherical coordinates
+///
 func psi2pxSph( r: Double, theta: Double, phi: Double) -> Double {
     let coeff = 1.0 / (4.0 * sqrt(2.0 * Double.pi))
     let linear = r
@@ -35,6 +52,9 @@ func psi2pxSph( r: Double, theta: Double, phi: Double) -> Double {
     return coeff * linear * expon * ang
 }
 
+/// convertPhi
+///
+/// ensures that the correct value of phi is given due to limitations of arctan
 func convertPhi(x: Double, y: Double, z: Double) -> Double {
     if (x > 0) {
         return atan(y/x)
